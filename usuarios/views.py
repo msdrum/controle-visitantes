c07f3754529934982389011f3d42db75764d5665
Marcio Silva <marciosilvadrum@gmail.com>
2024-04-12 23:16:23 -0300

    adding variables to index.html using context

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    context = {
+        "nome_pagina": "Início da dashboard",
+    }
+
+    return render(request, 'index.html', context)
