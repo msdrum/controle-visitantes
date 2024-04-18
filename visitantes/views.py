12f68fe639a724579ce5758bf0f456559aaaeba5
Marcio Silva <marciosilvadrum@gmail.com>
2024-04-17 23:03:56 -0300

    rendering form in the html file

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from visitantes.forms import VisitanteForm
 
 # Create your views here.
 def registrar_visitante(request):
 
-    context = {}
+    form = VisitanteForm()
+
+    context = {
+        "nome_pagina": "Registrar Visitante",
+        "form": form
+    }
 
     return render(request, "registrar_visitante.html", context)
 
