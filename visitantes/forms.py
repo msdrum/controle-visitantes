180273db91a4204e31d999905f60517a7e54d3dd
Marcio Silva <marciosilvadrum@gmail.com>
2024-04-20 19:28:23 -0300

    config error messages

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -8,3 +8,18 @@ class VisitanteForm(forms.ModelForm):
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo"
         ]
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro"
+            },
+            "cpf": {
+                "required": "O cpf do visitante é obrigatório para o registro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatória para o registro",
+                "invalid": "Insira um formato de data válido (dd/mm/aaaa)"
+            },
+            "numero_casa": {
+                "required": "Por favor, insira o número da casa a ser visitada"
+            },        
+        }
