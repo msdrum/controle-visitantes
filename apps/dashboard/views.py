11e0ec9fd3cf587dd4473dec523353a6cf8529d3
Marcio Silva <marciosilvadrum@gmail.com>
2024-05-30 21:52:40 -0300

    organizing the month visiting by descendent order

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from visitantes.models import Visitante
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
 
-    todos_visitantes = Visitante.objects.all()
+    todos_visitantes = Visitante.objects.order_by(
+        "-horario_chegada"
+    )
 
     visitantes_aguardando = todos_visitantes.filter(
         status="AGUARDANDO"
@@ -18,12 +21,20 @@ def index(request):
         status="FINALIZADO"
     )
 
+    hora_atual = timezone.now()
+    mes_atual = hora_atual.month
+
+    visitantes_mes = todos_visitantes.filter(
+        horario_chegada__month=mes_atual
+    )
+
     context = {
         "nome_pagina": "Início da dashboard",
         "todos_visitantes": todos_visitantes,
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizado": visitantes_finalizado.count(),
+        "visitantes_mes": visitantes_mes.count(),
         
     }
 
